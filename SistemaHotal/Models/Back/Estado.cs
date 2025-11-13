1ee8e6481959379cc3c0106df566bd5dafb6753a
Gimblet <gimbletdev@gmail.com>
2025-11-13 13:54:32 -0500

    refactor: Removed sistemaHotel
    Sistema Hotel Project was removed as it was an unfinished version of the Hotel project

--- SistemaHotal/Models/Back/Estado.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemaHotal.Models.Back
-{
-    public class Estado
-    {
-        public int id { get; set; }
-        public string? estado { get; set; }
-    }
-}
--- SistemaHotal/Models/Back/TipoDoc.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemaHotal.Models.Back
-{
-    public class TipoDoc
-    {
-        public int id { get; set; }
-        public string? tipo { get; set; }
-    }
-}
